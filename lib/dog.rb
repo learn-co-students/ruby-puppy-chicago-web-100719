ea9610f447327250a4d26eff64fcbafd3a2634b4
github NikolaCzv <github nikola.raicic@hotmail.com>
2019-10-20 15:19:38 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,27 +1,28 @@
-
 class Dog
-    attr_accessor :name
-  
-    @@all = []
-  
-    def initialize(name)
-      @name = name
-      save
-    end
-  
-    def self.all
-      @@all
-    end
-  
-    def self.print_all
-      puts @@all.map{ |dog| dog.name }
-    end
-  
-    def self.clear_all
-      @@all.clear
-    end
-  
-    def save
-      @@all << self
-    end
+
+  attr_accessor :name
+
+  @@all = []
+
+
+  def initialize(name)
+    @name = name
+    save
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.print_all
+    puts self.all.map { |dog| dog.name }
+  end
+
+  def save 
+    @@all << self
+  end
+end
